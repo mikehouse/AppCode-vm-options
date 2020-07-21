91f7c2fa86d463f969538fab9f0f7f9fa2f29305
Mikhail Demidov <mike.house.nsk@gmail.com>
2020-07-21 11:17:25 +0700

    Update ram_disk_mapper.sh

--- Map DerivedData to RAM/ram_disk_mapper.sh
+++ Map DerivedData to RAM/ram_disk_mapper.sh
@@ -4,7 +4,7 @@ VOLUME_NAME=ramdisk
 VOLUME_ROOT=/Volumes/$VOLUME_NAME
 
 if [ ! -d "$VOLUME_ROOT" ]; then
-	diskutil erasevolume HFS+ $VOLUME_NAME `hdiutil attach -nomount ram://44011200` # 16 GB DISC SIZE
+	diskutil erasevolume HFS+ $VOLUME_NAME `hdiutil attach -nomount ram://24000000` # 12.35 GB DISC SIZE
 fi
 
 XCODE_DERIVED_DATA_RAM_PATH=/Volumes/$VOLUME_NAME/Xcode/DerivedData
@@ -24,117 +24,52 @@ fi
 
 # AppCode Support
 
-APPCODE_VERSION=2019.1
-APPCODE_ROOT=~/Library/Caches/AppCode$APPCODE_VERSION
+for ver in "2019.1" "2019.2" "2019.3"; do
+    APPCODE_VERSION=$ver
+    APPCODE_ROOT=~/Library/Caches/AppCode$APPCODE_VERSION
 
-if [ -d "$APPCODE_ROOT" ]; then
+    if [ -d "$APPCODE_ROOT" ]; then
 
-	APPCODE_DERIVED_DATA_RAM_PATH=/Volumes/$VOLUME_NAME/$APPCODE_VERSION/DerivedData
+        APPCODE_DERIVED_DATA_RAM_PATH=/Volumes/$VOLUME_NAME/$APPCODE_VERSION/DerivedData
 
-	if [ ! -d "$APPCODE_DERIVED_DATA_RAM_PATH" ]; then
-		mkdir -p "$APPCODE_DERIVED_DATA_RAM_PATH"
-	fi
-
-
-	APPCODE_DERIVED_DATA_ORIGIN_PATH=$APPCODE_ROOT/DerivedData
-
-	if [ ! -L "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
-		if [ -d "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
-			rm -fr "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
-		fi	
-		ln -s "$APPCODE_DERIVED_DATA_RAM_PATH" "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
-	fi
-
-fi
-
-APPCODE_VERSION=2019.2
-APPCODE_ROOT=~/Library/Caches/AppCode$APPCODE_VERSION
-
-if [ -d "$APPCODE_ROOT" ]; then
-
-	APPCODE_DERIVED_DATA_RAM_PATH=/Volumes/$VOLUME_NAME/$APPCODE_VERSION/DerivedData
-
-	if [ ! -d "$APPCODE_DERIVED_DATA_RAM_PATH" ]; then
-		mkdir -p "$APPCODE_DERIVED_DATA_RAM_PATH"
-	fi
+        if [ ! -d "$APPCODE_DERIVED_DATA_RAM_PATH" ]; then
+            mkdir -p "$APPCODE_DERIVED_DATA_RAM_PATH"
+        fi
 
 
-	APPCODE_DERIVED_DATA_ORIGIN_PATH=$APPCODE_ROOT/DerivedData
+        APPCODE_DERIVED_DATA_ORIGIN_PATH=$APPCODE_ROOT/DerivedData
 
-	if [ ! -L "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
-		if [ -d "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
-			rm -fr "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
-		fi	
-		ln -s "$APPCODE_DERIVED_DATA_RAM_PATH" "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
-	fi
-	
-fi
+        if [ ! -L "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
+            if [ -d "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
+                rm -fr "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
+            fi	
+            ln -s "$APPCODE_DERIVED_DATA_RAM_PATH" "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
+        fi
 
-APPCODE_VERSION=2019.3
-APPCODE_ROOT=~/Library/Caches/AppCode$APPCODE_VERSION
+    fi
+done
 
-if [ -d "$APPCODE_ROOT" ]; then
+for ver in "2020.1" "2020.2"; do
+    APPCODE_VERSION=$ver
+    APPCODE_ROOT=~/Library/Caches/JetBrains/AppCode$APPCODE_VERSION
 
-	APPCODE_DERIVED_DATA_RAM_PATH=/Volumes/$VOLUME_NAME/$APPCODE_VERSION/DerivedData
+    if [ -d "$APPCODE_ROOT" ]; then
 
-	if [ ! -d "$APPCODE_DERIVED_DATA_RAM_PATH" ]; then
-		mkdir -p "$APPCODE_DERIVED_DATA_RAM_PATH"
-	fi
+        APPCODE_DERIVED_DATA_RAM_PATH=/Volumes/$VOLUME_NAME/$APPCODE_VERSION/DerivedData
 
-
-	APPCODE_DERIVED_DATA_ORIGIN_PATH=$APPCODE_ROOT/DerivedData
-
-	if [ ! -L "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
-		if [ -d "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
-			rm -fr "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
-		fi	
-		ln -s "$APPCODE_DERIVED_DATA_RAM_PATH" "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
-	fi
-	
-fi
-
-APPCODE_VERSION=2020.1
-APPCODE_ROOT=~/Library/Caches/JetBrains/AppCode$APPCODE_VERSION
-
-if [ -d "$APPCODE_ROOT" ]; then
-
-	APPCODE_DERIVED_DATA_RAM_PATH=/Volumes/$VOLUME_NAME/$APPCODE_VERSION/DerivedData
-
-	if [ ! -d "$APPCODE_DERIVED_DATA_RAM_PATH" ]; then
-		mkdir -p "$APPCODE_DERIVED_DATA_RAM_PATH"
-	fi
+        if [ ! -d "$APPCODE_DERIVED_DATA_RAM_PATH" ]; then
+            mkdir -p "$APPCODE_DERIVED_DATA_RAM_PATH"
+        fi
 
 
-	APPCODE_DERIVED_DATA_ORIGIN_PATH=$APPCODE_ROOT/DerivedData
+        APPCODE_DERIVED_DATA_ORIGIN_PATH=$APPCODE_ROOT/DerivedData
 
-	if [ ! -L "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
-		if [ -d "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
-			rm -fr "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
-		fi	
-		ln -s "$APPCODE_DERIVED_DATA_RAM_PATH" "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
-	fi
-	
-fi
-
-APPCODE_VERSION=2020.2
-APPCODE_ROOT=~/Library/Caches/JetBrains/AppCode$APPCODE_VERSION
-
-if [ -d "$APPCODE_ROOT" ]; then
-
-	APPCODE_DERIVED_DATA_RAM_PATH=/Volumes/$VOLUME_NAME/$APPCODE_VERSION/DerivedData
-
-	if [ ! -d "$APPCODE_DERIVED_DATA_RAM_PATH" ]; then
-		mkdir -p "$APPCODE_DERIVED_DATA_RAM_PATH"
-	fi
-
-
-	APPCODE_DERIVED_DATA_ORIGIN_PATH=$APPCODE_ROOT/DerivedData
-
-	if [ ! -L "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
-		if [ -d "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
-			rm -fr "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
-		fi	
-		ln -s "$APPCODE_DERIVED_DATA_RAM_PATH" "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
-	fi
-	
-fi
+        if [ ! -L "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
+            if [ -d "$APPCODE_DERIVED_DATA_ORIGIN_PATH" ]; then
+                rm -fr "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
+            fi	
+            ln -s "$APPCODE_DERIVED_DATA_RAM_PATH" "$APPCODE_DERIVED_DATA_ORIGIN_PATH"
+        fi
+        
+    fi
+done
